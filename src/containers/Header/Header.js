61e85c8149f35deed7bf4466c2fd8a6b6b2acaa1
Ivan <i.romanov@redgroupdigital.com>
2020-03-03 11:36:52 -0800

    Updated design solution

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -99,8 +99,8 @@ class Header extends Component {
         ref={this.headerRef}
       >
         <Container className={css.wrapper}>
-          <Link to='/header.json'>
-            <img src='https://via.placeholder.com/120x50.jpg' alt='Логотип компании'/>
+          <Link to='/' style={{ fontSize: 0 }}>
+            <img className={css.logo} src='https://via.placeholder.com/120x50.jpg' alt='Логотип компании'/>
           </Link>
           <button className={css.burger} type='button' onClick={this.handleClickBurger}>
             Управление адаптивным меню
