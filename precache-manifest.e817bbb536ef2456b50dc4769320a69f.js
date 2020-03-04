8023793ba5554454e8fedfd3cb2260fb51b68970
Ivan <i.romanov@redgroupdigital.com>
2020-03-04 10:47:50 -0800

    Updates

---
+++ precache-manifest.e817bbb536ef2456b50dc4769320a69f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ee92f037370bc5f74d0456b73134a8",
+    "url": "/kudos/index.html"
+  },
+  {
+    "revision": "e519b3c21b7200fb57dd",
+    "url": "/kudos/static/css/2.698563d4.chunk.css"
+  },
+  {
+    "revision": "1a65db16510caba51258",
+    "url": "/kudos/static/css/main.fdba86cf.chunk.css"
+  },
+  {
+    "revision": "e519b3c21b7200fb57dd",
+    "url": "/kudos/static/js/2.09c6ab3c.chunk.js"
+  },
+  {
+    "revision": "cc9c9fc963f9574279b557a5300ac537",
+    "url": "/kudos/static/js/2.09c6ab3c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a65db16510caba51258",
+    "url": "/kudos/static/js/main.2b2620e2.chunk.js"
+  },
+  {
+    "revision": "817d27da75af639a062c",
+    "url": "/kudos/static/js/runtime-main.6318405f.js"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/kudos/static/media/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "55df1df203dd0ab0956554d5acb84e79",
+    "url": "/kudos/static/media/Gilroy-Bold.55df1df2.woff2"
+  },
+  {
+    "revision": "073937d154640689b34e1ee3db908e96",
+    "url": "/kudos/static/media/Peace Sans Webfont.073937d1.woff2"
+  },
+  {
+    "revision": "7ba8163fef7a7cde4660dfc8a17b74e2",
+    "url": "/kudos/static/media/Peace-Sans-Webfont.7ba8163f.woff"
+  },
+  {
+    "revision": "4f2b2ed2943d4b19496951f01c843bf0",
+    "url": "/kudos/static/media/Roboto-Bold.4f2b2ed2.woff2"
+  },
+  {
+    "revision": "6552efbabcefb6d6695f238b6c6620c1",
+    "url": "/kudos/static/media/Roboto-Bold.6552efba.woff"
+  },
+  {
+    "revision": "056dc044ac904ef5b895e0dc8ebf4154",
+    "url": "/kudos/static/media/Roboto-Light.056dc044.woff"
+  },
+  {
+    "revision": "16695312a549d284e5ef5b20cc9c58cb",
+    "url": "/kudos/static/media/Roboto-Light.16695312.woff2"
+  },
+  {
+    "revision": "3c5527473962295b79be7b9ceb2b9084",
+    "url": "/kudos/static/media/Roboto-Medium.3c552747.woff2"
+  },
+  {
+    "revision": "7350337b673e86f2d62ee0eb5761e1e2",
+    "url": "/kudos/static/media/Roboto-Medium.7350337b.woff"
+  },
+  {
+    "revision": "1cfd1aa3b4fe4bba10c132acea9985af",
+    "url": "/kudos/static/media/Roboto-Regular.1cfd1aa3.woff"
+  },
+  {
+    "revision": "2222f1fd23aa2c08af158311d680ac4a",
+    "url": "/kudos/static/media/Roboto-Regular.2222f1fd.woff2"
+  },
+  {
+    "revision": "4b7e2b338a9f48003ddd0825153d57a5",
+    "url": "/kudos/static/media/Roboto-Thin.4b7e2b33.woff2"
+  },
+  {
+    "revision": "a67e8502e37efeb8a9494b254dfd324a",
+    "url": "/kudos/static/media/Roboto-Thin.a67e8502.woff"
+  },
+  {
+    "revision": "1ac128dc58e0110ef4ceb36ff2ff9041",
+    "url": "/kudos/static/media/beltheder-palmyra.1ac128dc.woff2"
+  },
+  {
+    "revision": "623289f86a290c321cfafd592d2afef8",
+    "url": "/kudos/static/media/beltheder-palmyra.623289f8.woff"
+  }
+]);
